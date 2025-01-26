7384c9ea07dd4b86c2ff07c22adb38b2c80a3172
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 12:39:37 +0530

    Add files via upload

---
+++ Areaofrectangle.java
@@ -0,0 +1,15 @@
+public class Areaofrectangle {
+    public static void main(String[] args) {
+        // int length1 = 12;
+        // int breadth1 = 2;
+        // int area =length1 * breadth1;
+        // System.out.println("the area of reactangle is :" + area);
+    
+float length =20.2f;
+float breadth = 10.2f;
+float area = length*breadth;
+System.out.println("the area of reactangle is :" + area);
+    }
+}
+    
+
